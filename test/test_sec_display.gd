cc3e23bc0b0e5198b5e9b8fe34c0a10d6be0b262
gosia <gosia@coiai.net>
2026-09-14 09:59:00 +0900

    test fix

--- test/test_sec_display.gd
+++ test/test_sec_display.gd
@@ -1,16 +1,19 @@
 extends GutTest
 
 func test_0():
-	_verify(0, "00:00")
+	_verify(0, "00:00.000")
 
 func test_less_than_minute():
-	_verify(33, "00:33")
+	_verify(33000, "00:33.000")
 
 func test_minute():
-	_verify(60, "01:00")
+	_verify(60000, "01:00.000")
 
 func test_more_than_minute():
-	_verify(73, "01:13")
+	_verify(73000, "01:13.000")
+
+func test_millis_more_than_minute():
+	_verify(73365, "01:13.365")
 
 func _verify(sec: int, expected: String):
-	assert_eq(Globals.seconds_to_string(sec), expected)
+	assert_eq(Globals.millis_to_string(sec), expected)
